176f66b15fb4cde3e7097e449e8718f173d4e62c
Guntash Kaur <83450329+GuntashKaur@users.noreply.github.com>
2022-08-24 16:56:52 +0530

    initial commit

---
+++ isTriangle.js
@@ -0,0 +1,15 @@
+const angles = document.querySelectorAll(".angle");
+
+const isTrianglebtn = document.querySelector("#isTriangle");
+const formData = document.querySelector(".form")
+
+function sumOfAngles(angle1, angle2, angle3){
+
+    
+}
+
+function clickHandler(){
+    const sum = sumOfAngles(45, 45, 90)
+}
+
+isTrianglebtn.addEventListener("click", clickHandler);
