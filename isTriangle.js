0d5ad231c4523e80035c210cd30dd898c3ca4aa2
Guntash Kaur <83450329+GuntashKaur@users.noreply.github.com>
2022-08-26 16:52:45 +0530

    updatetd

--- isTriangle.js
+++ isTriangle.js
@@ -1,18 +1,24 @@
 const angles = document.querySelectorAll(".angle");
-
 const isTrianglebtn = document.querySelector("#isTriangle");
-const formData = document.querySelector(".form")
+const outputDiv = document.querySelector(".outputDiv");
+
 
 function sumOfAngles(angle1, angle2, angle3){
 
     const result = angle1 + angle2+ angle3;
-    console.log(result)
-
-    
+    return result;  
 }
 
+
 function clickHandler(){
-    const sum = sumOfAngles(45, 45, 90)
+    const sum = sumOfAngles(Number(angles[0].value) , Number(angles[1].value) , Number(angles[2].value));
+
+    if(sum === 180){
+        outputDiv.innerText("Angles form a triangle")
+    }
+    else{
+        outputDiv.innerText("Angles do not form a triangle")
+    }
 }
 
 isTrianglebtn.addEventListener("click", clickHandler);
